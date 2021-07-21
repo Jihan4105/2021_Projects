7c0d3c2d743f926a0577cd9d8acdb2a13d0be06d
Jihan4105 <wlgks6979@naver.com>
2021-07-21 22:45:47 +0900

    2021 07 21 React-First

--- server/server.js
+++ server/server.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const app = express();
-//---- 추가 ----------------------------------------------------
 const cors = require('cors');
-//-------------------------------------------------------------
+
 const port = 3001;
 const movie_router = require('./router/movie_router');
 
-//---- 추가 ----------------------------------------------------
 app.use(cors());
-//-------------------------------------------------------------
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/movies', movie_router);
 
-//---- 수정 ----------------------------------------------------
 app.get('/server', (req, res) => {
-//-------------------------------------------------------------
    res.send('Hello Express'); 
 });
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
+
